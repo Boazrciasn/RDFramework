65b754d8f7a9fe1a49d02969bab495beeba1f7c0
bezero <n.maxudov@gmail.com>
2016-03-01 15:09:12 +0200

    screen size settings

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "include/mainwindow.h"
 #include "QApplication"
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
     MainWindow w;
+
+    QDesktopWidget dw;
+    int x=dw.width()*0.3;
+    int y=dw.height()*0.9;
+    w.setFixedSize(x,y);
+
     w.show();
     return app.exec();
 }
