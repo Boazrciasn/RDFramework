344f3081fe7724a62b537b410da74d91577726e4
Barış Özcan <barisozcan89@gmail.com>
2016-06-22 13:19:03 +0300

    minor change

--- src/RandomDecisionForest.cpp
+++ src/RandomDecisionForest.cpp
@@ -19,7 +19,6 @@ void RandomDecisionForest::readTrainingImageFiles()
     reader->findImages(m_dir, "", fNames, m_DS.m_trainlabels);
     m_numOfLetters = fNames.size();
     qDebug() << "NO OF LETTERS : " << m_numOfLetters;
-
     for (auto filePath : fNames)
     {
         cv::Mat image = cv::imread(filePath.toStdString(), CV_LOAD_IMAGE_GRAYSCALE);
@@ -27,7 +26,6 @@ void RandomDecisionForest::readTrainingImageFiles()
                            m_params.probDistX, m_params.probDistX, cv::BORDER_CONSTANT);
         m_DS.m_trainImagesVector.push_back(image);
     }
-
     qDebug() << "No of IMAGES : " << m_DS.m_trainImagesVector.size() <<
              " NO of Fnames" << m_numOfLetters << "Images Vector Size : " <<
              m_DS.m_trainImagesVector.size();
@@ -42,7 +40,6 @@ void RandomDecisionForest::readTestImageFiles()
     std::vector<QString> fNames;
     auto *reader = new Reader();
     reader->findImages(m_dir, "", fNames, m_DS.m_testlabels);
-
     for (auto filePath : fNames)
     {
         //TODO: reads file with only
@@ -52,7 +49,6 @@ void RandomDecisionForest::readTestImageFiles()
                            m_params.probDistX, m_params.probDistX, cv::BORDER_CONSTANT);
         m_DS.m_testImagesVector.push_back(image);
     }
-
     qDebug() << "No of test IMAGES : " << m_DS.m_testImagesVector.size();
     fNames.clear();
 }
@@ -66,20 +62,15 @@ void RandomDecisionForest::readAndIdentifyWords()
     // source file for average values
     QString lineOffsetFile = "./combined.txt";
     QFile input(lineOffsetFile);
-
     if(!input.open(QIODevice::ReadOnly))
         std::cout <<
                   "RandomDecisionForest::readAndIdentifyWords failed to open file! \n";
-
     QString formattedOutputFile = "./formatted_output.txt";
     QFile output(formattedOutputFile);
-
     if(!output.open(QIODevice::WriteOnly))
         std::cout <<
                   "RandomDecisionForest::readAndIdentifyWords failed to open file! \n";
-
     auto lineNo = 0;
-
     for (auto filePath : fNames)
     {
         //read offset line
@@ -101,7 +92,6 @@ void RandomDecisionForest::readAndIdentifyWords()
         QVector<quint32> fgPxNumberPerCol;
         cv::Mat layeredImage = getLayeredHist(m_DS.m_testImagesVector[0], 0,
                                               fgPxNumberPerCol);
-
         for(QRect wordRoi : wordsRoi)
         {
             cv::Rect layeredWordRoi = cv::Rect(wordRoi.x(), wordRoi.y(),
@@ -127,7 +117,6 @@ void RandomDecisionForest::readAndIdentifyWords()
             output.write("\n");
         }
     }
-
     output.close();
     input.close();
     fNames.clear();
@@ -139,27 +128,21 @@ void RandomDecisionForest::searchWords(QString query, int queryId)
     // source file for average values
     QString formattedInputFile = "./formatted_output.txt";
     QFile input(formattedInputFile);
-
     if(!input.open(QIODevice::ReadOnly))
         std::cout << "RandomDecisionForest::searchWords failed to open file! \n";
-
     QString endResultFile = "./end_result.txt";
     QFile output(endResultFile);
-
     if(!output.open(QIODevice::WriteOnly))
         std::cout << "RandomDecisionForest::searchWords failed to open file! \n";
-
     QStringList queryList = query.split(' ');
     //lineid, linecontent
     QMultiHash<int, QString> hash;
     auto segmentNo = 1;
-
     while (!input.atEnd())
     {
         QString offsetLine = "";
         int lineid;
         int segmentEnd = segmentNo + 5;
-
         do
         {
             //read offset line
@@ -167,12 +150,10 @@ void RandomDecisionForest::searchWords(QString query, int queryId)
             QStringList myStringList = offsetLine.split(' ');
             lineid = myStringList[2].split(':')[0].toInt();
             QString word = myStringList[0];
-
             if(lineid <= segmentEnd)
                 hash.insert(lineid, offsetLine);
         }
         while(lineid <= segmentEnd && !input.atEnd());
-
         //TODO make ordered.
         //check for matches
         for(QString search : queryList)
@@ -180,13 +161,11 @@ void RandomDecisionForest::searchWords(QString query, int queryId)
             for(int line_id = segmentNo; line_id <= segmentEnd; ++line_id)
             {
                 QMultiHash<int, QString>::const_iterator i = hash.find(line_id);
-
                 while (i != hash.end() && i.key() == line_id)
                 {
                     QString details = i.value();
                     QStringList myStringList = details.split(' ');
                     QString word = myStringList[0];
-
                     if(word == search)
                     {
                         QString conf_bbox = " " + myStringList[1] + " " + myStringList[2];
@@ -194,16 +173,13 @@ void RandomDecisionForest::searchWords(QString query, int queryId)
                         output.write(" " + QByteArray::number(segmentNo));
                         output.write(conf_bbox.toStdString().c_str());
                     }
-
                     ++i;
                 }
             }
         }
-
         hash.remove(segmentNo++);
         hash.insert(lineid, offsetLine);
     }
-
     hash.clear();
     output.close();
     input.close();
@@ -217,7 +193,6 @@ void RandomDecisionForest::imageToPixels(std::vector<pixel_ptr> &res,
 {
     int nRows = image.rows;
     int nCols = image.cols;
-
     for(int i = 0; i < nRows; ++i)
     {
         for(int j = 0; j < nCols; ++j)
@@ -249,7 +224,6 @@ cv::Mat RandomDecisionForest::colorCoder(const cv::Mat &labelImage,
     int nCols = InputImage.cols;
     int labelCount = m_params.labelCount;
     cv::Mat color(nRows, nCols, CV_8UC1);
-
     //create color code Mat accoridng to of each pixel.
     for(int i = 0; i < nRows; ++i)
     {
@@ -259,7 +233,6 @@ cv::Mat RandomDecisionForest::colorCoder(const cv::Mat &labelImage,
             color.at<double>(i, j) = (label / labelCount) * 255;
         }
     }
-
     //create 3 channel image with 2 channels with same intensity values of cv::Mat color :
     std::vector<cv::Mat> Image_channels(3);
     Image_channels.at(0) = InputImage;
@@ -274,7 +247,6 @@ cv::Mat RandomDecisionForest::colorCoder(const cv::Mat &labelImage,
 void RandomDecisionForest::trainForest()
 {
     double cpu_time;
-
     //#pragma omp parallel for num_threads(8)
     for (int i = 0; i < m_params.nTrees; ++i)
     {
@@ -302,7 +274,6 @@ void RandomDecisionForest::trainForest()
         qDebug() << " Train time of the current Tree : " << cpu_time;
         //        }
     }
-
     qDebug() << "Forest Size : " << m_forest.size();
 }
 
@@ -322,47 +293,37 @@ cv::Mat RandomDecisionForest::getLayeredHist(cv::Mat test_image, int index,
     imageinfo_ptr img_Info(new ImageInfo(" ", index));
     int rangeRows = nRows - probDistY;
     int rangeCols = nCols - probDistX;
-
     for(int c = probDistX; c < rangeCols; ++c)
     {
         int col_index = (c - probDistX) * labelCount;
         int fgPxCount = 0;
-
         for(int r = probDistY; r < rangeRows; ++r)
         {
             cv::Mat probHist = cv::Mat::zeros(1, labelCount, cv::DataType<float>::type);
             auto intensity = test_image.at<uchar>(r, c);
-
             if (intensity == 0)
             {
                 placeHistogram(layeredHist, probHist, r - probDistY, col_index);
             }
-
             else
             {
                 ++fgPxCount;
                 pixel_ptr px(new Pixel(Coord(r, c), intensity, img_Info));
                 auto nForest = m_forest.size();
-
                 for(unsigned int i = 0; i < nForest; ++i)
                 {
                     node_ptr leaf = m_forest[i]->getLeafNode(m_DS, px, 0);
                     probHist += leaf->m_hist;
                 }
-
                 //Normalize the Histrograms
                 float sum = cv::sum(probHist)[0];
-
                 if( sum != 0 )
                     probHist /= sum;
-
                 placeHistogram(layeredHist, probHist, r - probDistY, col_index);
             }
         }
-
         fgPxNumberPerCol[col_index / labelCount] = fgPxCount;
     }
-
     return layeredHist;
 }
 
@@ -370,7 +331,6 @@ void RandomDecisionForest::placeHistogram(cv::Mat &output,
                                           const cv::Mat &pixelHist, int pos_row, int pos_col)
 {
     auto range = pos_col + m_params.labelCount;
-
     for (int i = pos_col; i < range ; ++i)
     {
         output.at<float>(pos_row, i) = pixelHist.at<float>(i - pos_col);
@@ -378,7 +338,8 @@ void RandomDecisionForest::placeHistogram(cv::Mat &output,
 }
 
 // confidenceMat : LabelCount x testImage.rows : each row displays likelooh for each column of a label.
-cv::Mat_<float> RandomDecisionForest::createLetterConfidenceMatrix(const cv::Mat &layeredHist, const QVector<quint32> &fgPxNumberPerCol)
+cv::Mat_<float> RandomDecisionForest::createLetterConfidenceMatrix(const cv::Mat &layeredHist,
+                                                                   const QVector<quint32> &fgPxNumberPerCol)
 {
     int labelcount = m_params.labelCount;
     int layered_Cols = layeredHist.cols;
@@ -386,14 +347,12 @@ cv::Mat_<float> RandomDecisionForest::createLetterConfidenceMatrix(const cv::Mat
     cv::Mat_<float> confidenceMat(labelcount, confmat_cols);
     int i_row;
     int i_col;
-
     for (int i = 0; i < layered_Cols; ++i)
     {
         i_row = i % labelcount;
         i_col = i / labelcount;
         confidenceMat.at<float>(i_row, i_col) = cv::sum(layeredHist.col(i))[0];
     }
-
     Util::averageLHofCol(confidenceMat, fgPxNumberPerCol);
     //    Util::plot(confidenceMat.row(0),m_parent);
     return confidenceMat;
@@ -404,7 +363,6 @@ void RandomDecisionForest::test()
 {
     int nImages = m_DS.m_testImagesVector.size();
     qDebug() << "Number of Test images:" << QString::number(nImages);
-
     for(auto i = 0; i < nImages; ++i)
     {
         QVector<quint32> fgPxNumberPerCol;
@@ -418,7 +376,6 @@ void RandomDecisionForest::test()
         Util::getWordWithConfidence(confidenceMat, 26, word, conf);
         qDebug() << "Word extracted & conf: " << word << "  " << 100 * conf;
     }
-
     //    m_accuracy = Util::calculateAccuracy(m_DS.m_testlabels, classify_res);
     //    emit resultPercentage(m_accuracy);
 }
