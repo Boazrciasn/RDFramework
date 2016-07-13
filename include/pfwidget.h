50a8607c3cbb73b3940933b5bc53db24850b7f8a
Nekruzjon Maxudov <nekruz@lovendo.co>
2016-07-13 17:43:47 +0300

    add mediaplayer libs

--- include/pfwidget.h
+++ include/pfwidget.h
@@ -4,6 +4,12 @@
 
 #include <QWidget>
 
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QFileDialog>
+#include <QProgressBar>
+#include <QSlider>
+
 namespace Ui {
 class PFWidget;
 }
@@ -16,8 +22,15 @@ public:
     explicit PFWidget(QWidget *parent = 0);
     ~PFWidget();
 
+private slots:
+    void on_actionOpen_triggered();
+
 private:
     Ui::PFWidget *ui;
+    QMediaPlayer* mPlayer;
+    QVideoWidget* mVideoWidg;
+    QProgressBar* mProgressBar;
+    QSlider* mSlider;
 };
 
 #endif // PFWIDGET_H
--- src/pfwidget.cpp
+++ src/pfwidget.cpp
@@ -6,9 +6,19 @@ PFWidget::PFWidget(QWidget *parent) :
     ui(new Ui::PFWidget)
 {
     ui->setupUi(this);
+
+    mPlayer = new QMediaPlayer(this);
+    mVideoWidg = new QVideoWidget(this);
+    mPlayer->setVideoOutput(mVideoWidg);
 }
 
 PFWidget::~PFWidget()
 {
     delete ui;
+}
+
+void PFWidget::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Open a File","","Video File (*.avi, *.mpg, *.mp4)");
+
 }
